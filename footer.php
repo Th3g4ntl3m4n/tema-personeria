1da212593b5c8e7669d2b504e106a93a0919f0ac
emanosalva <emanosalva40@unab.edu.co>
2020-12-28 15:11:44 -0500

    agregue muchas x

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
         <a class="navbar-brand" href="#">
             <img src="assets/logo.png" alt="" width="150" height="50">
           </a>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste expedita enim dolor deleniti officia laborum nulla cumque assumenda. Quia id odit ex autem tenetur nostrum, incidunt perspiciatis exercitationem ab error?</p>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste expedita enim dolor deleniti officia laborum nulla xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxcumque assumenda. Quia id odit ex autem tenetur nostrum, incidunt perspiciatis exercitationem ab error?</p>
        </div>
    </div>
    <!-- Fin Footer -->
